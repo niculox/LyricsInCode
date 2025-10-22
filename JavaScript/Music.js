cf85bfb2cb9fe56ce55f808a0a46ea8ca7424c5a
niculox <nicolasamaral1997@gmail.com>
2025-10-22 16:40:50 -0300

    Add contribution guidelines to Music files for community participation

---
+++ JavaScript/Music.js
@@ -0,0 +1,16 @@
+/*
+
+Sua contribuição é muito bem-vinda! Este projeto é feito pela comunidade e para a comunidade.
+Se você tem uma música favorita e quer implementá-la em sua linguagem preferida, siga estes passos:
+
+1. Faça um Fork deste repositório (clique no botão "Fork" no canto superior direito).
+2. Crie uma nova Branch para sua música (git checkout -b minha-musica-nova).
+3. Adicione sua música:
+    Siga a estrutura de pastas Linguagem/Musica.ext.
+4. Se a pasta da linguagem ou do artista ainda não existir, sinta-se à vontade para criá-la!
+5. O arquivo deve conter a letra da música. Seja criativo! Você pode usar comentários, strings, ou até fazer um programa que imprime a letra de forma estilizada.
+6. Faça o Commit de suas mudanças (git commit -m 'Adiciona [Musica] em [Linguagem]').
+7. Envie sua Branch (git push origin minha-musica-nova).
+8. Abra um Pull Request no GitHub.
+
+*/
